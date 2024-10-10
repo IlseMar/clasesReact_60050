fee75fb918744ee405633715dd03a69000307749
ilse mar <ilse1991@gmail.com>
2024-10-10 15:03:35 -0600

    APIs

---
+++ Clase7-API/clase7/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [characters, setCharacters] = useState([]);
+
+  //Obtener los primeros 20 personajes de la API de Rick and Morty
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setCharacters(data.results);
+      });
+
+    fetch("http://localhost:4000")
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <>
+      <div>{characters.length && characters.map()}</div>
+      <ItemDetailContainer />
+    </>
+  );
+}
+export default App;
